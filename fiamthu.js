f6970ec51ab40a0391051c7fcd5039a6e0e8ebed
Fakea15 <75936459+Fakea15@users.noreply.github.com>
2021-07-23 22:43:41 +0530

    Add files via upload

---
+++ fiamthu.js
@@ -0,0 +1,138 @@
+var icon = document.getElementById("icon");
+var icon0 = document.getElementById("icon0");
+var icon1 = document.getElementById("icon1");
+var icon2 = document.getElementById("icon2");
+var icon3 = document.getElementById("icon3");
+var icon4 = document.getElementById("icon4");
+var icon5 = document.getElementById("icon5");
+var icon6 = document.getElementById("icon6");
+var icon7 = document.getElementById("icon7");
+var icon8 = document.getElementById("icon8");
+var icon9 = document.getElementById("icon9");
+var icon10 = document.getElementById("icon10");
+var James1 = document.getElementById("James1");
+var AlbertFtMaste = document.getElementById("AlbertFtMaste");
+var CosteaFeatMastea = document.getElementById("CosteaFeatMastea");
+var James2 = document.getElementById("James2");
+var Khawtea = document.getElementById("Khawtea");
+var Lalchhanhima1 = document.getElementById("Lalchhanhima1");
+var Lalchhanhima2 = document.getElementById("Lalchhanhima2");
+var Laltanpuia1 = document.getElementById("Laltanpuia1");
+var mapuiaLehLray = document.getElementById("mapuiaLehLray");
+var Mapuia1 = document.getElementById("Mapuia1");
+var Mapuia2 = document.getElementById("Mapuia2");
+var PuThangthuama1 = document.getElementById("PuThangthuama1");
+
+icon.onclick = function(){
+    if(James1.paused) {
+        James1.play();
+        icon.src = "pause.png";
+    } else {
+        James1.pause();
+        icon.src = "play.png";
+    }
+}
+icon0.onclick = function(){
+    if(AlbertFtMaste.paused) {
+        AlbertFtMaste.play();
+        icon0.src = "pause.png";
+    } else {
+        AlbertFtMaste.pause();
+        icon0.src = "play.png"
+    }
+}
+icon1.onclick = function(){
+    if(CosteaFeatMastea.paused) {
+        CosteaFeatMastea.play();
+        icon1.src = "pause.png";
+    } else {
+        CosteaFeatMastea.pause();
+        icon1.src = "play.png"
+    }
+}
+icon2.onclick = function(){
+    if(James2.paused) {
+        James2.play();
+        icon2.src = "pause.png";
+    } else {
+        James2.pause();
+        icon2.src = "play.png"
+    }
+}
+icon3.onclick = function(){
+    if(Khawtea.paused) {
+        Khawtea.play();
+        icon3.src = "pause.png";
+    } else {
+        Khawtea.pause();
+        icon3.src = "play.png"
+    }
+}
+icon4.onclick = function(){
+    if(Lalchhanhima1.paused) {
+        Lalchhanhima1.play();
+        icon4.src = "pause.png";
+    } else {
+        Lalchhanhima1.pause();
+        icon4.src = "play.png";
+    }
+}
+icon5.onclick = function(){
+    if(Lalchhanhima2.paused) {
+        Lalchhanhima2.play();
+        icon5.src = "pause.png";
+    } else {
+        Lalchhanhima2.pause();
+        icon5.src = "play.png";
+    }
+}
+icon6.onclick = function() {
+    if(Laltanpuia1.paused) {
+        Laltanpuia1.play();
+        icon6.src = "pause.png";
+    } else {
+        Laltanpuia1.pause();
+        icon6.src = "play.png";
+    }
+}
+icon7.onclick = function() {
+    if(mapuiaLehLray.paused) {
+        mapuiaLehLray.play();
+        icon7.src = "pause.png";
+    } else {
+        mapuiaLehLray.pause();
+        icon7.src = "play.png";
+    }
+}
+icon8.onclick = function() {
+    if(Mapuia1.paused) {
+        Mapuia1.play();
+        icon8.src = "pause.png";
+    } else {
+        Mapuia1.pause();
+        icon8.src = "play.png";
+    }
+}
+icon9.onclick = function() {
+    if(Mapuia2.paused) {
+        Mapuia2.play();
+        icon9.src = "pause.png";
+    } else {
+        Mapuia2.pause();
+        icon9.src = "play.png";
+    }
+}
+icon10.onclick = function() {
+    if(PuThangthuama1.paused) {
+        PuThangthuama1.play();
+        icon10.src = "pause.png";
+    } else {
+        PuThangthuama1.pause();
+        icon10.src = "play.png";
+    }
+}
+
+
+
+
+
---
+++ lengzem.js
@@ -0,0 +1,194 @@
+var icon1 = document.getElementById("icon1");
+var icon2 = document.getElementById("icon2");
+var icon3 = document.getElementById("icon3");
+var icon4 = document.getElementById("icon4");
+var icon5 = document.getElementById("icon5");
+var icon6 = document.getElementById("icon6");
+var icon7 = document.getElementById("icon7");
+var icon8 = document.getElementById("icon8");
+var icon9 = document.getElementById("icon9");
+var icon10 = document.getElementById("icon10");
+var icon11 = document.getElementById("icon11");
+var icon12 = document.getElementById("icon12");
+var icon13 = document.getElementById("icon13");
+var icon14 = document.getElementById("icon14");
+var icon15 = document.getElementById("icon15");
+var icon16 = document.getElementById("icon16");
+var icon17 = document.getElementById("icon17");
+
+var IsraelLalbiakrema1 = document.getElementById("IsraelLalbiakrema1");
+var IsraelLalbiakrema2 = document.getElementById("IsraelLalbiakrema2");
+var KHminga1 = document.getElementById("KHminga1");
+var KHminga2 = document.getElementById("KHminga2");
+var Kimkima1 = document.getElementById("Kimkima1");
+var Kimkima2 = document.getElementById("Kimkima2");
+var Lalnunsanga = document.getElementById("Lalnunsanga");
+var Lilzo1 = document.getElementById("Lilzo1");
+var Lilzo2 = document.getElementById("Lilzo2");
+var TriauTrackx = document.getElementById("TriauTrackx");
+var VanHleihSung = document.getElementById("VanHleihSung");
+var ZauteaFeatZonunBand = document.getElementById("ZauteaFeatZonunBand");
+var MazualiK = document.getElementById("MazualiK");
+var FelaColney = document.getElementById("FelaColney");
+var JMalsawma = document.getElementById("JMalsawma");
+var BenjaminSum = document.getElementById("BenjaminSum");
+var BenjaminSum2 = document.getElementById("BenjaminSum2");
+
+icon1.onclick = function(){
+    if(IsraelLalbiakrema1.paused) {
+        IsraelLalbiakrema1.play();
+        icon1.src = "pause.png";
+    } else {
+        IsraelLalbiakrema1.pause();
+        icon1.src = "play.png"
+    }
+}
+icon2.onclick = function(){
+    if(IsraelLalbiakrema2.paused) {
+        IsraelLalbiakrema2.play();
+        icon2.src = "pause.png";
+    } else {
+        IsraelLalbiakrema2.pause();
+        icon2.src = "play.png";
+    }
+}
+icon3.onclick = function(){
+    if(KHminga1.paused) {
+        KHminga1.play();
+        icon3.src = "pause.png";
+    } else {
+        KHminga1.pause();
+        icon3.src = "play.png"
+    }
+}
+icon4.onclick = function(){
+    if(KHminga2.paused) {
+        KHminga2.play();
+        icon4.src = "pause.png";
+    } else {
+        KHminga2.pause();
+        icon4.src = "play.png";
+    }
+}
+icon5.onclick = function(){
+    if(Kimkima1.paused) {
+        Kimkima1.play();
+        icon5.src = "pause.png";
+    } else {
+        Kimkima1.pause();
+        icon5.src = "play.png";
+    }
+}
+icon6.onclick = function() {
+    if(Kimkima2.paused) {
+        Kimkima2.play();
+        icon6.src = "pause.png";
+    } else {
+        Kimkima2.pause();
+        icon6.src = "play.png";
+    }
+}
+icon7.onclick = function() {
+    if(Lalnunsanga.paused) {
+        Lalnunsanga.play();
+        icon7.src = "pause.png";
+    } else {
+        Lalnunsanga.pause();
+        icon7.src = "play.png";
+    }
+}
+icon8.onclick = function() {
+    if(Lilzo1.paused) {
+        Lilzo1.play();
+        icon8.src = "pause.png";
+    } else {
+        Lilzo1.pause();
+        icon8.src = "play.png";
+    }
+}
+icon9.onclick = function() {
+    if(Lilzo2.paused) {
+        Lilzo2.play();
+        icon9.src = "pause.png";
+    } else {
+        Lilzo2.pause();
+        icon9.src = "play.png";
+    }
+}
+icon10.onclick = function() {
+    if(TriauTrackx.paused) {
+        TriauTrackx.play();
+        icon10.src = "pause.png";
+    } else {
+        TriauTrackx.pause();
+        icon10.src = "play.png";
+    }
+}
+icon11.onclick = function(){
+    if(VanHleihSung.paused) {
+        VanHleihSung.play();
+        icon11.src = "pause.png";
+    } else {
+        VanHleihSung.pause();
+        icon11.src = "play.png"
+    }
+}
+icon12.onclick = function(){
+    if(ZauteaFeatZonunBand.paused) {
+        ZauteaFeatZonunBand.play();
+        icon12.src = "pause.png";
+    } else {
+        ZauteaFeatZonunBand.pause();
+        icon12.src = "play.png"
+    }
+}
+icon13.onclick = function(){
+    if(MazualiK.paused) {
+        MazualiK.play();
+        icon13.src = "pause.png";
+    } else {
+        MazualiK.pause();
+        icon13.src = "play.png"
+    }
+}
+icon14.onclick = function(){
+    if(FelaColney.paused) {
+        FelaColney.play();
+        icon14.src = "pause.png";
+    } else {
+        FelaColney.pause();
+        icon14.src = "play.png";
+    }
+}
+icon15.onclick = function(){
+    if(JMalsawma.paused) {
+        JMalsawma.play();
+        icon15.src = "pause.png";
+    } else {
+        JMalsawma.pause();
+        icon15.src = "play.png"
+    }
+}
+icon16.onclick = function(){
+    if(BenjaminSum.paused) {
+        BenjaminSum.play();
+        icon16.src = "pause.png";
+    } else {
+        BenjaminSum.pause();
+        icon16.src = "play.png";
+    }
+}
+icon17.onclick = function(){
+    if(BenjaminSum2.paused) {
+        BenjaminSum2.play();
+        icon17.src = "pause.png";
+    } else {
+        BenjaminSum2.pause();
+        icon17.src = "play.png";
+    }
+}
+
+
+
+
+
